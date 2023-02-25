88a316bfd25c89a3503eae16aedf06f9d239e0ff
Josmi Jose <josmijose100@gmail.com>
2023-02-24 17:59:56 -0800

    Add header

---
+++ hair-with-flair/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React   from 'react';
+import hairsalon from "../images/hairsalon.jpg";
+export default function Home() {
+    return (
+      <div id="container">
+      <h2 calse="sidepadding">About Us</h2>
+        <div class="AboutUs">
+        <div class="side sidepadding" style={{width:'30%'}}>
+          <img class="image" src={hairsalon} alt="hair-salon"/>
+        </div>
+        <div class="side">
+          <p class="aboutText">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+        </div>
+      </div>
+      </div>
+    );
+  };
